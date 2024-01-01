7eb29a637d2ddac67083f8952241a9d45e4b10e1
tema5002 <152618947+tema5002@users.noreply.github.com>
2024-01-01 19:14:02 +0300

    Update temalib.py

--- temalib.py
+++ temalib.py
@@ -1,6 +1,5 @@
 from random import randint
-import codecs
-import os
+import codecs, os
 
 def openfile(file):
     return codecs.open(file, encoding="utf-8")
@@ -8,8 +7,10 @@ def openfile(file):
 def editfile(file):
     return codecs.open(file, "w", encoding="utf-8")
 
-def get_file_path(folder, filename):
+def smthfile(file, readingmode):
+    return codecs.open(file, readingmode, encoding="utf-8")
 
+def get_file_path(folder, filename):
     folder_dir = os.path.dirname(__file__)
 
     # creates the filename folder if it doesnt exist
@@ -17,47 +18,29 @@ def get_file_path(folder, filename):
     if not os.path.exists(folder_dir):
         os.makedirs(folder_dir)
 
-    h = os.path.join(folder_dir, filename)
-    if not os.path.exists(h):
-        with open(h, "w") as f: pass
+    filepath = os.path.join(folder_dir, filename)
+    if not os.path.exists(filepath):
+        with open(filepath, "w") as f: pass
 
-    return h
+    return filepath
 
-# add line to the end of the file:
 def altteotf(filepath, line):
-    file_list = openfile(filepath)
-    with editfile(filepath) as file:
-        if file_list != ['']
-            for every in file_list:
-                file.write(every)
-            file.write("\n" + line)
-        else: file.write(line)
+    with smthfile(filepath, "a+") as file:
+        file.seek(0)
+        if file.read():
+            file.seek(0, 2)
+            file.write("\n")
+        file.write(line)
 
-def bin(n,c):
-    if n==0: return c
-    c+=str(bin(n//2,c))
-    c+=str(n%2)
-    return c
-
-def randomip():
-    return f"{randint(0,255)}.{randint(0,255)}.{randint(0,255)}.{randint(0,255)}"
-
-def isWord(letter):
-    bool=""
-    for number in range(len(letter)):
-        for every in "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz":
-            if letter[number]==every:
-                bool+="t"
-                break
-            if every=="z": bool+="f"
-    if bool.find("f")>0: return False
-    else: return True
+def isWord(word):
+    h = 0
+    for every in word:
+        if any(every==_ for _ in "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"):
+            h += 1
+    return (len(word)==h)
 
 def isLetter(letter):
-    for every in "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz":
-        if letter==every: return True
-        if letter==every: break
-        if every=="z": return False
+    return any(_==letter for _ in "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 
 def hAutoCorrect(hInput):
     hAutoCorrected=""
@@ -88,7 +71,7 @@ def generate_ip(name):
         if num>=100: num=str(num)[1:]
         ans+=str(num)
     ans=int(ans)
-    ans=bin(ans,"")
+    ans=int(bin(ans,"")[2:])
     dis=len(ans)/32
     rawAns=""
     for every in range(0,32):
